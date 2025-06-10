3338982d0dae486159ec3447bd3fc0b3734d9072
이채빈 <dlcoqls002@gmail.com>
2025-06-10 17:37:10 +0900

    SFML

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Timber!");
+   
+    sf::Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+
+    sf::Sprite sprirteBackground;
+    sprirteBackground.setTexture(textureBackground);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.draw(sprirteBackground);
+
+        window.display();
+    }
+
+    return 0;
+}
